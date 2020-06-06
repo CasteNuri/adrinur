a79b7b3961f058f4e699c93bb44b4ddbc49388fa
casteNuri <nuricaste26@gmail.com>
2020-06-06 10:36:25 +0200

    1st commit

---
+++ adrinur/src/main/java/com/adrinur/springboot/backend/AdrinurApplication.java
@@ -0,0 +1,13 @@
+package com.adrinur.springboot.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdrinurApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdrinurApplication.class, args);
+	}
+
+}
