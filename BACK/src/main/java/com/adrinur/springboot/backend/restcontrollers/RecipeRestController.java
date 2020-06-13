33a03d88390156b6ed259683533222aa2f942ff5
casteNuri <nuricaste26@gmail.com>
2020-06-13 17:48:56 +0200

    error 500

--- BACK/src/main/java/com/adrinur/springboot/backend/restcontrollers/RecipeRestController.java
+++ BACK/src/main/java/com/adrinur/springboot/backend/restcontrollers/RecipeRestController.java
@@ -47,18 +47,16 @@ public class RecipeRestController {
 		
 		try {
 			recipe = recipeServices.getRecipeById(id);
-			
+			if (recipe == null || id <= 0 || id == null) {
+				errorResponse.put("mensaje", "El cliente con ID".concat(id.toString().concat(" no existe")));
+				return new ResponseEntity<Map<String,Object>>(errorResponse, HttpStatus.NOT_FOUND);
+	        }
 		} catch (DataAccessException e) {
 			errorResponse.put("mensaje", "Error al acceder a la base de datos");
 			errorResponse.put("error", e.getMessage().concat(": ")
 					.concat(e.getMostSpecificCause().getMessage()));
 			return new ResponseEntity<Map<String,Object>>(errorResponse,HttpStatus.INTERNAL_SERVER_ERROR);
 		}
-		
-		if (recipe == null || id <= 0 || id == null) {
-			errorResponse.put("mensaje", "El cliente con ID".concat(id.toString().concat(" no existe")));
-			return new ResponseEntity<Map<String,Object>>(errorResponse, HttpStatus.NOT_FOUND);
-        }
 		
 		recipeDto = modelMapper.map(recipe, RecipeDto.class);
 		return new ResponseEntity<RecipeDto>(recipeDto, HttpStatus.OK);
