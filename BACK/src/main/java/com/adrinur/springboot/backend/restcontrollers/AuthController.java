3481b4ec3db032cadab43783a640bdf92b301244
casteNuri <nuricaste26@gmail.com>
2020-06-16 18:02:06 +0200

    crossOrigin Auth

--- BACK/src/main/java/com/adrinur/springboot/backend/restcontrollers/AuthController.java
+++ BACK/src/main/java/com/adrinur/springboot/backend/restcontrollers/AuthController.java
@@ -9,6 +9,7 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -25,6 +26,7 @@ import com.adrinur.springboot.backend.utils.SecurityUtils;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 
+@CrossOrigin(origins= {"http://localhost:4200"})
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
